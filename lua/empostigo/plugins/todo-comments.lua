7d033b97e8df62e65673bc64c1a990e1b8d39afa
empostigo <emmanuel.postigo@tutanota.com>
2024-06-23 10:29:19 +0200

    Finish neovim configuration

---
+++ lua/empostigo/plugins/todo-comments.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/todo-comments.nvim",
+  event = { "BufReadPRe", "BufNewFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local todo_comments = require("todo-comments")
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "]t", function()
+      todo_comments.jump_next()
+    end, { desc = "Next todo comments" })
+
+    keymap.set("n", "[t", function()
+      todo_comments.jump_prev()
+    end, { desc = "Previous todo comments" })
+
+    todo_comments.setup()
+  end,
+}
+
