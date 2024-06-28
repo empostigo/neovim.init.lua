3f8fe8ab88cbee7f922cae48b50f1e07c4edceb2
empostigo <emmanuel.postigo@tutanota.com>
2024-06-28 14:04:50 +0200

    Add toggle terminal mapping

--- lua/empostigo/plugins/toggleterm.lua
+++ lua/empostigo/plugins/toggleterm.lua
@@ -1,3 +1,9 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      vim.keymap.set("n", "<leader>tt", ":ToggleTerm size=16<CR>", { desc = "Open terminal" }),
+    },
+  },
 }
