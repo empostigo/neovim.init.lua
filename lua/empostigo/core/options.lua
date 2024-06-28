f5b71b10709bb636d79217c1f04b5256595dab1b
empostigo <emmanuel.postigo@tutanota.com>
2024-06-28 13:51:12 +0200

    Add toggleterm plugin

--- lua/empostigo/core/options.lua
+++ lua/empostigo/core/options.lua
@@ -2,6 +2,7 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
 
+-- line numbers
 opt.relativenumber = true
 opt.number = true
 
@@ -22,7 +23,7 @@ opt.cursorline = true
 -- turn on termguicolors for tokyonight colorscheme to work
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- alow backspace on indent, end of line or insert mode start position
@@ -34,4 +35,4 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-vim.opt.showmode = false   -- hide mode in native status line
+vim.opt.showmode = false -- hide mode in native status line
